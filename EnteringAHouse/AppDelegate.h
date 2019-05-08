dec4591f3a49e7394302370b9cf12645c6514029
Dayson Dong <Daysond@outlook.com>
2019-05-08 15:31:15 -0400

    Initial Commit

---
+++ EnteringAHouse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EnteringAHouse
+//
+//  Created by Dayson Dong on 2019-05-08.
+//  Copyright © 2019 Dayson Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
